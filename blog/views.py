2f873dc38107fbf4c7789d0be0865392ca5ac5d5
Vivek-R-Pawar <vivekpawar189@gmail.com>
2021-02-13 11:24:42 +0530

    second commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def blogHome(request):
+    return HttpResponse("This is blog home. We will keep all the blogpost here!")
+
+
+def blogPost(request, slug):
+    return HttpResponse(f"This is blogPost: {slug}")
